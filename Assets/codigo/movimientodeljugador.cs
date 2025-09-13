38ef3f8bd966a669dbb2174b4ccf9a8239eba93f
est.diana.camargo3 <est.diana.camargo3@unimilitar.edu.co>
2025-09-12 22:17:27 -0500

    agregar jugador circulo

---
+++ Assets/codigo/movimientodeljugador.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class movimientodeljugador : MonoBehaviour
+{
+    public float velocidad = 5f; // Velocidad del jugador
+    private Rigidbody2D rb;
+    private Vector2 movimiento;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // Buscar el Rigidbody2D del jugador
+    }
+
+    void Update()
+    {
+        // Teclas WASD o Flechas
+        movimiento.x = Input.GetAxisRaw("Horizontal");
+        movimiento.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Mover al jugador con físicas
+        rb.MovePosition(rb.position + movimiento * velocidad * Time.fixedDeltaTime);
+    }
+}
